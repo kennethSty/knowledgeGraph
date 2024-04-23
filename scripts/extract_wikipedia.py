f141943036eb576f7cfad05ab1bee0cbc854c3ab
Kenneth Styppa <kenneth.styppa@web.de>
2024-04-23 13:51:29 +0200

    added extraction structure

--- scripts/extract_wikipedia.py
+++ scripts/extract_wikipedia.py
@@ -1,4 +1,5 @@
-
+#3rd party packages
+import pandas as pd
 # Intra-project packages
 from utils.extract_utils import set_user_agent, set_lang, search_wiki
 
@@ -6,11 +7,13 @@ from utils.extract_utils import set_user_agent, set_lang, search_wiki
 search_params = {
     'list': 'search',
     'srprop': '',
-    'srlimit': 10,  # Maximum number of results per query
-    'limit': 10,  # Maximum number of results to return
+    'srlimit': 1,  # Maximum number of results per query
+    'limit': 1,  # Maximum number of results to return
     'srsearch': "Krankheit",
     'sroffset': 0  # Starting offset for pagination
 }
 
 #extract articles
 pages = search_wiki(search_params=search_params, batch_proc=True)
+pages_df = pd.DataFrame(pages)
+pages_df.to_csv(f"../data/pages_until_sroff_{search_params['sroffset']}.csv", encoding="utf-8", index=False)
